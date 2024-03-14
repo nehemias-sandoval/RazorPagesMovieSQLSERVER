668bc628718057d41109dbbd50abced860bd9cd6
f208 <f208@STCF208010EF>
2024-03-13 19:11:59 -0600

    Agregar archivos de proyecto.

---
+++ RazorPagesMovieSQLSERVER/Data/RazorPagesMovieSQLSERVERContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovieSQLSERVER.Models;
+
+namespace RazorPagesMovieSQLSERVER.Data
+{
+    public class RazorPagesMovieSQLSERVERContext : DbContext
+    {
+        public RazorPagesMovieSQLSERVERContext (DbContextOptions<RazorPagesMovieSQLSERVERContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovieSQLSERVER.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ RazorPagesMovieSQLSERVER/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovieSQLSERVER.Data;
+using RazorPagesMovieSQLSERVER.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieSQLSERVERContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieSQLSERVERContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieSQLSERVERContext' not found.")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
